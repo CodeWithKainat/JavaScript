13fc1b031af25db5f3bfcbbad9b2d07c4d9225f1
CodeWithKainat <kk4710070@gmail.com>
2023-04-07 02:31:10 +0500

    Initial

---
+++ app.js
@@ -0,0 +1,15 @@
+// First Day
+
+// var num1 = 3;
+// var num2 = 7;
+
+// alert(num1 + num2);
+
+var drivingAge = prompt("What is your age ?")
+
+if (drivingAge >=18){
+    alert("You are allowed to drive")
+}
+else{
+    alert("You are not allowed to drive")
+}
